09702298e2eb5070b40ebefaba290f414c57c113
NorvinBansilan <74772792+NorvinBansilan@users.noreply.github.com>
2025-12-07 16:54:34 +0800

    Add files via upload

---
+++ Simulation 1-50.R
@@ -0,0 +1,179 @@
+setwd("~/Desktop/Schisto2/SEI/Simulation 1-50/Simulation 1/A")
+source('codes/Japonicum.R')
+
+cat('§ Running simulations\n')
+japonicum = schistoSEI(PARAMS)
+
+cat('§ Plotting output\n')
+##--CHildren
+source('codes/1children/far/ALL_children_far.R')
+source('codes/1children/near/ALL_children_near.R')
+##--Adults
+source('codes/2adults/far/ALL_adults_far.R')
+source('codes/2adults/near/ALL_adults_near.R')
+##--Animal
+source('codes/3animals/free roaming/ALL_animal_free roaming.R')
+source('codes/3animals/non-free roaming/ALL_animal_non-free roaming.R')
+##--Environment
+source('codes/4environment/miracidia/ALL_envi_miracidia.R')
+source('codes/4environment/cercaria/ALL_envi_cercaria.R')
+source('codes/4environment/snails/ALL_envi_snails.R')
+
+cat('§ Table output\n')
+source('codes/tables.R')
+
+################################################################################
+
+setwd("~/Desktop/Schisto2/SEI/Simulation 1-50/Simulation 1/B")
+source('codes/Japonicum.R')
+
+cat('§ Running simulations\n')
+japonicum = schistoSEI(PARAMS)
+
+cat('§ Plotting output\n')
+##--CHildren
+source('codes/1children/far/ALL_children_far.R')
+source('codes/1children/near/ALL_children_near.R')
+##--Adults
+source('codes/2adults/far/ALL_adults_far.R')
+source('codes/2adults/near/ALL_adults_near.R')
+##--Animal
+source('codes/3animals/free roaming/ALL_animal_free roaming.R')
+source('codes/3animals/non-free roaming/ALL_animal_non-free roaming.R')
+##--Environment
+source('codes/4environment/miracidia/ALL_envi_miracidia.R')
+source('codes/4environment/cercaria/ALL_envi_cercaria.R')
+source('codes/4environment/snails/ALL_envi_snails.R')
+
+cat('§ Table output\n')
+source('codes/tables.R')
+
+################################################################################
+
+setwd("~/Desktop/Schisto2/SEI/Simulation 1-50/Simulation 1/C")
+source('codes/Japonicum.R')
+
+cat('§ Running simulations\n')
+japonicum = schistoSEI(PARAMS)
+
+cat('§ Plotting output\n')
+##--CHildren
+source('codes/1children/far/ALL_children_far.R')
+source('codes/1children/near/ALL_children_near.R')
+##--Adults
+source('codes/2adults/far/ALL_adults_far.R')
+source('codes/2adults/near/ALL_adults_near.R')
+##--Animal
+source('codes/3animals/free roaming/ALL_animal_free roaming.R')
+source('codes/3animals/non-free roaming/ALL_animal_non-free roaming.R')
+##--Environment
+source('codes/4environment/miracidia/ALL_envi_miracidia.R')
+source('codes/4environment/cercaria/ALL_envi_cercaria.R')
+source('codes/4environment/snails/ALL_envi_snails.R')
+
+cat('§ Table output\n')
+source('codes/tables.R')
+
+################################################################################
+
+setwd("~/Desktop/Schisto2/SEI/Simulation 1-50/Simulation 1/D")
+source('codes/Japonicum.R')
+
+cat('§ Running simulations\n')
+japonicum = schistoSEI(PARAMS)
+
+cat('§ Plotting output\n')
+##--CHildren
+source('codes/1children/far/ALL_children_far.R')
+source('codes/1children/near/ALL_children_near.R')
+##--Adults
+source('codes/2adults/far/ALL_adults_far.R')
+source('codes/2adults/near/ALL_adults_near.R')
+##--Animal
+source('codes/3animals/free roaming/ALL_animal_free roaming.R')
+source('codes/3animals/non-free roaming/ALL_animal_non-free roaming.R')
+##--Environment
+source('codes/4environment/miracidia/ALL_envi_miracidia.R')
+source('codes/4environment/cercaria/ALL_envi_cercaria.R')
+source('codes/4environment/snails/ALL_envi_snails.R')
+
+cat('§ Table output\n')
+source('codes/tables.R')
+
+################################################################################
+
+setwd("~/Desktop/Schisto2/SEI/Simulation 1-50/Simulation 1/E")
+source('codes/Japonicum.R')
+
+cat('§ Running simulations\n')
+japonicum = schistoSEI(PARAMS)
+
+cat('§ Plotting output\n')
+##--CHildren
+source('codes/1children/far/ALL_children_far.R')
+source('codes/1children/near/ALL_children_near.R')
+##--Adults
+source('codes/2adults/far/ALL_adults_far.R')
+source('codes/2adults/near/ALL_adults_near.R')
+##--Animal
+source('codes/3animals/free roaming/ALL_animal_free roaming.R')
+source('codes/3animals/non-free roaming/ALL_animal_non-free roaming.R')
+##--Environment
+source('codes/4environment/miracidia/ALL_envi_miracidia.R')
+source('codes/4environment/cercaria/ALL_envi_cercaria.R')
+source('codes/4environment/snails/ALL_envi_snails.R')
+
+cat('§ Table output\n')
+source('codes/tables.R')
+
+################################################################################
+
+setwd("~/Desktop/Schisto2/SEI/Simulation 1-50/Simulation 1/A human chemo 1st M")
+source('codes/Japonicum.R')
+
+cat('§ Running simulations\n')
+japonicum = schistoSEI(PARAMS)
+
+cat('§ Plotting output\n')
+##--CHildren
+source('codes/1children/far/ALL_children_far.R')
+source('codes/1children/near/ALL_children_near.R')
+##--Adults
+source('codes/2adults/far/ALL_adults_far.R')
+source('codes/2adults/near/ALL_adults_near.R')
+##--Animal
+source('codes/3animals/free roaming/ALL_animal_free roaming.R')
+source('codes/3animals/non-free roaming/ALL_animal_non-free roaming.R')
+##--Environment
+source('codes/4environment/miracidia/ALL_envi_miracidia.R')
+source('codes/4environment/cercaria/ALL_envi_cercaria.R')
+source('codes/4environment/snails/ALL_envi_snails.R')
+
+cat('§ Table output\n')
+source('codes/tables.R')
+
+################################################################################
+
+setwd("~/Desktop/Schisto2/SEI/Simulation 1-50/Simulation 1/B human chemo mid M")
+source('codes/Japonicum.R')
+
+cat('§ Running simulations\n')
+japonicum = schistoSEI(PARAMS)
+
+cat('§ Plotting output\n')
+##--CHildren
+source('codes/1children/far/ALL_children_far.R')
+source('codes/1children/near/ALL_children_near.R')
+##--Adults
+source('codes/2adults/far/ALL_adults_far.R')
+source('codes/2adults/near/ALL_adults_near.R')
+##--Animal
+source('codes/3animals/free roaming/ALL_animal_free roaming.R')
+source('codes/3animals/non-free roaming/ALL_animal_non-free roaming.R')
+##--Environment
+source('codes/4environment/miracidia/ALL_envi_miracidia.R')
+source('codes/4environment/cercaria/ALL_envi_cercaria.R')
+source('codes/4environment/snails/ALL_envi_snails.R')
+
+cat('§ Table output\n')
+source('codes/tables.R')
