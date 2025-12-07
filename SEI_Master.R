a8c4b650696838a754eba674e1263c819f65520d
NorvinBansilan <74772792+NorvinBansilan@users.noreply.github.com>
2025-12-07 16:55:57 +0800

    Add files via upload

---
+++ SEI_Master.R
@@ -0,0 +1,103 @@
+setwd("~/Desktop/Schisto2")
+##---- set up
+cat('§ Setup\n')
+source('setup/setup_variables.R')
+source('setup/setup_parameters.R')
+source('setup/setup_combinations5.R')
+# source('setup/setup_combinations3.R')
+# source('setup/setup_combinations2.R')
+# source('setup/setup_combinations.R')
+
+######-----------------------------------------------------------------------
+#numSteps = 365 * (1/0.01) ##delta = 0.01
+#numSteps = 1095 * (1/0.01) ##delta = 0.01
+numSteps = 2920 * (1/0.01) ##delta = 0.01
+#####------------------------------------------------------------------------
+
+setwd("~/Desktop/Schisto2/SEI")
+source('ALL/Simulation1-50.R')
+
+setwd("~/Desktop/Schisto2/SEI")
+source('ALL/Simulation51-100.R')
+
+setwd("~/Desktop/Schisto2/SEI")
+source('ALL/Simulation101-150.R')
+
+setwd("~/Desktop/Schisto2/SEI")
+source('ALL/Simulation151-200.R')
+
+setwd("~/Desktop/Schisto2/SEI")
+source('ALL/Simulation201-250.R')
+
+setwd("~/Desktop/Schisto2/SEI")
+source('ALL/Simulation251-300.R')
+
+setwd("~/Desktop/Schisto2/SEI")
+source('ALL/Simulation301-350.R')
+
+# setwd("~/Desktop/Schisto2/SEI")
+# source('ALL/Simulation351-400.R')
+# 
+# setwd("~/Desktop/Schisto2/SEI")
+# source('ALL/Simulation401-450.R')
+# 
+# setwd("~/Desktop/Schisto2/SEI")
+# source('ALL/Simulation451-500.R')
+
+# setwd("~/Desktop/Schisto2/SEI")
+# source('ALL/Simulation501-550.R')
+# 
+# setwd("~/Desktop/Schisto2/SEI")
+# source('ALL/Simulation551-600.R')
+# 
+# setwd("~/Desktop/Schisto2/SEI")
+# source('ALL/Simulation601-650.R')
+# 
+# setwd("~/Desktop/Schisto2/SEI")
+# source('ALL/Simulation651-700.R')
+# 
+# setwd("~/Desktop/Schisto2/SEI")
+# source('ALL/Simulation701-750.R')
+# 
+# setwd("~/Desktop/Schisto2/SEI")
+# source('ALL/Simulation751-800.R')
+# 
+# setwd("~/Desktop/Schisto2/SEI")
+# source('ALL/Simulation801-850.R')
+# 
+# setwd("~/Desktop/Schisto2/SEI")
+# source('ALL/Simulation851-900.R')
+# 
+# setwd("~/Desktop/Schisto2/SEI")
+# source('ALL/Simulation901-950.R')
+# 
+# setwd("~/Desktop/Schisto2/SEI")
+# source('ALL/Simulation951-1000.R')
+# 
+# setwd("~/Desktop/Schisto2/SEI")
+# source('ALL/Simulation1001-1050.R')
+# 
+# setwd("~/Desktop/Schisto2/SEI")
+# source('ALL/Simulation1051-1100.R')
+# 
+# setwd("~/Desktop/Schisto2/SEI")
+# source('ALL/Simulation1101-1150.R')
+# 
+# setwd("~/Desktop/Schisto2/SEI")
+# source('ALL/Simulation1151-1200.R')
+# 
+# setwd("~/Desktop/Schisto2/SEI")
+# source('ALL/Simulation1201-1250.R')
+
+# setwd("~/Desktop/Schisto2/SEI")
+# source('ALL/Simulation1251-1300.R')
+# 
+# setwd("~/Desktop/Schisto2/SEI")
+# source('ALL/Simulation1301-1350.R')
+# 
+# setwd("~/Desktop/Schisto2/SEI")
+# source('ALL/Simulation1351-1400.R')
+# 
+# setwd("~/Desktop/Schisto2/SEI")
+# source('ALL/Simulation1401-1458.R')
+
